9d70a7bb3c317cb389039b490600e760cf72614e
harry-hm <funforumca@gmail.com>
2025-10-21 21:42:38 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -146,6 +146,59 @@ best = cost
 path = trip  
 print("Cost:", best, "Path:", path)  
 
+#7. Tower of Hanoi
+def honnai(n,source,aux,dest):
+    if n==0:
+        return
+    honnai(n-1,source,dest,aux)
+    print(f"Move disk {n} from source {source} to destination {dest}")
+    honnai(n-1,aux,source,dest)
+n=3
+honnai(n,'A','B','C')
+
+#8. Monkey
+def monkey(n):
+    climb=0
+    banana=0
+    hungry=True
+    for i in range(n):
+        if hungry:
+            climb+=1
+            banana+=1
+            hungry=False
+        else:
+            climb+=1
+            return climb ,banana
+n=10
+climb, banana=monkey(n)
+print(f"The Monkey made {climb} climbs and get {banana} Bananas")
+
+#9. Alpha Beta Purning
+MIN,MAX=-1000,1000
+def minimax(depth,nodeindex,maximizingPlayer,values,alpha,beta):
+    if depth==3:
+        return values[nodeindex]
+    if maximizingPlayer:
+        best=MIN
+        for i in range(0,2):
+            val=minimax(depth+1,nodeindex*2+i,False,values,alpha,beta)
+            best=max(best,val)
+            alpha=max(alpha,best)
+            if beta<=alpha:
+                break
+        return best
+    else:
+        best=MAX
+        for i in range(0,2):
+            val=minimax(depth+1,nodeindex*2+i,True,values,alpha,beta)
+            best=min(best,val)
+            beta=min(beta,best)
+            if beta<=alpha:
+                break
+        return best
+values=[3, 5, 6, 9, 1, 2, 0, -1]
+print("The optimal value is:",minimax(0,0,True,values,MIN,MAX))
+
 # 10. 8-Queens
 global N
 N=4
@@ -187,6 +240,24 @@ def solveNQ():
     printSolution(board)
     return True
 solveNQ()
+
+# 11. Chatbot
+import random
+responses=["Hello,how can I help you?",
+           "What do you want to talk about?",
+           "I'm not sure what you mean.",
+           "Can you repeat that?",
+           "I'm sorry,I don't understand.",
+           "Goodbye!"]
+def get_response():
+    return random.choice(responses)
+def start_chatbot():
+    print("Hello,I'm a chatbot.What do you want to talk about?")
+    while True:
+        user_input=input()
+        response=get_response()
+        print(response)
+start_chatbot()
 
 #12. Hangman
 import random
@@ -245,74 +316,3 @@ text="This ia a simple example to remove the stop words wsing NLTK."
 words=word_tokenize(text)
 fil=[w for w in words if w.lower() not in stopwords.words('english')]
 print(fil)
-
-#7. Tower of Hanoi
-def honnai(n,source,aux,dest):
-    if n==0:
-        return
-    honnai(n-1,source,dest,aux)
-    print(f"Move disk {n} from source {source} to destination {dest}")
-    honnai(n-1,aux,source,dest)
-n=3
-honnai(n,'A','B','C')
-
-#8. Monkey
-def monkey(n):
-    climb=0
-    banana=0
-    hungry=True
-    for i in range(n):
-        if hungry:
-            climb+=1
-            banana+=1
-            hungry=False
-        else:
-            climb+=1
-            return climb ,banana
-n=10
-climb, banana=monkey(n)
-print(f"The Monkey made {climb} climbs and get {banana} Bananas")
-
-#9. Alpha Beta Purning
-MIN,MAX=-1000,1000
-def minimax(depth,nodeindex,maximizingPlayer,values,alpha,beta):
-    if depth==3:
-        return values[nodeindex]
-    if maximizingPlayer:
-        best=MIN
-        for i in range(0,2):
-            val=minimax(depth+1,nodeindex*2+i,False,values,alpha,beta)
-            best=max(best,val)
-            alpha=max(alpha,best)
-            if beta<=alpha:
-                break
-        return best
-    else:
-        best=MAX
-        for i in range(0,2):
-            val=minimax(depth+1,nodeindex*2+i,True,values,alpha,beta)
-            best=min(best,val)
-            beta=min(beta,best)
-            if beta<=alpha:
-                break
-        return best
-values=[3, 5, 6, 9, 1, 2, 0, -1]
-print("The optimal value is:",minimax(0,0,True,values,MIN,MAX))
-
-# 11. Chatbot
-import random
-responses=["Hello,how can I help you?",
-           "What do you want to talk about?",
-           "I'm not sure what you mean.",
-           "Can you repeat that?",
-           "I'm sorry,I don't understand.",
-           "Goodbye!"]
-def get_response():
-    return random.choice(responses)
-def start_chatbot():
-    print("Hello,I'm a chatbot.What do you want to talk about?")
-    while True:
-        user_input=input()
-        response=get_response()
-        print(response)
-start_chatbot()
